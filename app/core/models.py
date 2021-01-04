56020c81bdd84ce6d5cf5b23f0f14cefca3db75b
Hubert Cieslewicz <h.cieslewicz@wp.pl>
2021-01-04 18:32:45 +0100

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
 class Usermanager(BaseUserManager):
@@ -10,6 +11,17 @@ class Usermanager(BaseUserManager):
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        """Creates and saves a new superuser"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.create_user(email, password)
+        user.is_stuff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
